1337fcf524c7923a00137f3f0fd1c3a4c6f3172b
Evan segaul <esegaul@gmail.com>
2022-06-25 19:15:33 -0400

    new schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,4 @@
--- DROP TABLE auth;
--- DROP TABLE ics_data;
-
-CREATE TABLE IF NOT EXISTS auth (
-    eth_address char(64) PRIMARY KEY,
-    secret_pass varchar(64) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS ics_data (
-    eth_address CHAR(64) PRIMARY KEY,
-    ics    BYTEA NOT NULL
+CREATE TABLE IF NOT EXISTS addr_pub_keys (
+    eth_address CHAR(42) PRIMARY KEY,
+    public_key VARCHAR(65) NOT NULL
 );
